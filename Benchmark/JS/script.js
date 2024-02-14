74de6c5c2d205ac29834e2aef0659c7688d8a0b5
Luca Grossi <lucagrossi87@gmail.com>
2024-02-14 09:32:04 +0100

    json-primoFix

--- Benchmark/JS/script.js
+++ Benchmark/JS/script.js
@@ -1,7 +1,9 @@
-const domande = fetch(
-  "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy"
-)
-  .then((response) => response.json())
-  .then((dati) => dati.results);
-
-console.log(domande);
+fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
+      .then((response) => response.json())
+      .then((dati) => {
+            let domande=dati.results
+            let show = document.createElement('p')
+            show.innerHTML=domande[0].question
+            document.body.append(show)
+            console.log(domande);
+      })
