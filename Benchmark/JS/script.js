37c280d42bca383468cc606eaebefce3a29aca7a
iosonodipe <dipecod@gmail.com>
2024-02-14 17:54:27 +0100

    funzione cambia pagina e riempimento div content

--- Benchmark/JS/script.js
+++ Benchmark/JS/script.js
@@ -1,9 +1,50 @@
 fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
-      .then((response) => response.json())
-      .then((dati) => {
-            let domande=dati.results
-            let show = document.createElement('p')
-            show.innerHTML=domande[0].question
-            document.body.append(show)
-            console.log(domande);
-      })
+  .then((response) => response.json())
+  .then((dati) => {
+    let domande = dati.results;
+
+    let punteggio = 5;
+
+    // function calcoloPercentualeCorrette() {
+    //   let percentualeGiuste = document.querySelector('#percentualeCorrette')
+    //   console.log(percentualeGiuste);
+    //   percentualeGiuste.innerText((punteggio/10)*100)
+    //   console.log(percentualeGiuste.innerText);
+    // }
+
+    // calcoloPercentualeCorrette()
+  });
+
+//variabili globali
+
+iframe.onload = function () {
+  const oldNode = iframe.contentWindow.document.getElementById("content");
+  let dynamicContainer = document.querySelector(".dynamic-container");
+  let clone = document.importNode(oldNode, true);
+  dynamicContainer.append(clone);
+};
+
+//SELETTORI GLOBALI
+let iframe = document.querySelector("iframe");
+
+//FUNZIONI
+
+//funzione cambio pagina (riempimento iframe con html)
+function cambioPagina() {
+  switch (iframe.src) {
+    case "./welcome.html":
+      iframe.src = "./test.html";
+      break;
+    case "./test.html":
+      iframe.src = "./results.html";
+      break;
+    case "./results.html":
+      iframe.src = "./review.html";
+      break;
+  }
+}
+
+// let bottoneProceed = iframe.contentDocument.querySelector("#proceed");
+// let bottoneProceed = document.querySelector("#proceed");
+// console.log(bottoneProceed);
+// bottoneProceed.addEventListener('click', cambioPagina)
